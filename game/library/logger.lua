9ac30d2cb7eee784e9126868e9694b9334d73322
Leon Jiang <titangate1202@gmail.com>
2013-05-31 23:54:22 -0700

    Replace original print with logger

--- game/library/logger.lua
+++ game/library/logger.lua
@@ -1,7 +1,9 @@
 Logger = {}
 
+local realprint = print
+
 function Logger._log(level,msg,...)
-	Logger._target = Logger._target or print
+	Logger._target = Logger._target or realprint
 	msg = string.format(msg,...)
 	msg = string.format("[%s:%s] %s",
 		debug.getinfo(3 + level or 0, "Sl").short_src,
@@ -15,19 +17,19 @@ function Logger.log(...)
 end
 
 function Logger.logWarning(msg,...)
-	Logger._target = Logger._target or print
+	Logger._target = Logger._target or realprint
 	msg = "Warning: "..string.format(msg,...)
 	Logger._log(0,msg)
 end
 
 function Logger.logWarningLevel(level,msg,...)
-	Logger._target = Logger._target or print
+	Logger._target = Logger._target or realprint
 	msg = "Warning: "..string.format(msg,...)
 	Logger._log(level,msg)
 end
 
 function Logger.logError(msg,...)
-	Logger._target = Logger._target or print
+	Logger._target = Logger._target or realprint
 	msg = "Error: "..string.format(msg,...)
 	Logger._log(0,msg)
 end
@@ -35,3 +37,5 @@ end
 function Logger.setTarget(target)
 	Logger._target = target
 end
+
+print = Logger.log
