68ec365ce9af1d20774712d904fac703535a47e2
Leon Jiang <titangate1202@gmail.com>
2013-06-02 22:43:21 -0700

    Reorganize Shader attribute transitions

--- game/shader/init.lua
+++ game/shader/init.lua
@@ -1,3 +1,20 @@
-Shader = {}
+Shader = Object:subclass'Shader'
+Shader.sharedObject = {}
+
+function Shader:getSharedObject()
+	Shader.sharedObject[self.class.name] = Shader.sharedObject[self.class.name] or {}
+	return Shader.sharedObject[self.class.name]
+end
+
+function Shader:setAttr(attr,target,duration,mode)
+	local attidx = self:getSharedObject().att2idx[attr]
+	local shader = self:getShader()
+	if duration then
+		return shader:seekAttr(attidx,target,duration,mode)
+	else
+		print 'immediate'
+		return shader:setAttr(attidx,target,duration,mode)
+	end
+end
 require 'shader.blur'
 require 'shader.vibrate'
--- game/shader/vibrate.lua
+++ game/shader/vibrate.lua
@@ -1,22 +1,34 @@
+Shader.Vibrate = Shader:subclass'Shader.Vibrate'
 
-function Shader.vibrate(obj)
-	local file
-	file = assert ( io.open ( 'shader/tex2d.vsh', mode ),"fail to read fragment shader")
-	local vsh = file:read ( '*all' )
-	file:close ()
+local Vibrate = Shader.Vibrate
+function Vibrate:initialize()
+	local obj = self:getSharedObject()
+	if not obj.shader then
+		local file
+		file = assert ( io.open ( 'shader/tex2d.vsh', mode ),"fail to read fragment shader")
+		local vsh = file:read ( '*all' )
+		file:close ()
 
-	file = assert ( io.open ( 'shader/vibrate.fsh', mode ),"fail to read vertex shader")
-	local fsh = file:read ( '*all' )
-	file:close ()
-	local shader = MOAIShader.new()
-	shader:reserveUniforms(1)
-	shader:declareUniform(1,'intensity',MOAIShader.UNIFORM_FLOAT)
-	shader:setAttr(1, 0)
-	shader:moveAttr(1, 5.0,5)
-	shader:setVertexAttribute ( 1, 'position' )
-	shader:setVertexAttribute ( 2, 'uv' )
-	shader:setVertexAttribute ( 3, 'color' )
-	shader:load ( vsh, fsh )
-	obj.prop:seekScl(5,1,5)
-	return shader
+		file = assert ( io.open ( 'shader/vibrate.fsh', mode ),"fail to read vertex shader")
+		local fsh = file:read ( '*all' )
+		file:close ()
+		local shader = MOAIShader.new()
+		shader:reserveUniforms(1)
+		shader:declareUniform(1,'intensity',MOAIShader.UNIFORM_FLOAT)
+		shader:setVertexAttribute ( 1, 'position' )
+		shader:setVertexAttribute ( 2, 'uv' )
+		shader:setVertexAttribute ( 3, 'color' )
+		shader:load ( vsh, fsh )
+		obj.shader = shader
+	end
+	if not obj.att2idx then
+		obj.att2idx = {
+			intensity = 1,
+		}
+	end
+end
+
+function Vibrate:getShader()
+	local obj = self:getSharedObject()
+	return obj.shader
 end
