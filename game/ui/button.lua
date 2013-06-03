6104942768e97464883e58f767d12f13c001f53a
Leon Jiang <titangate1202@gmail.com>
2013-06-02 21:09:51 -0700

    Clean up button class

--- game/ui/button.lua
+++ game/ui/button.lua
@@ -5,24 +5,6 @@ function Button:load(parent)
 	self.text = TextBoxActor()
 	self.text.quad = standardQuad(self.w,self.h)
 	self.text.style = self:getStyle().text
---[[
-	local text = MOAITextBox.new()
-	text:setString(b.title or 'BUTTON')
-	text:setTextSize(60,72)
-	text:setYFlip(true)
-	assert(style.font)
-	text:setFont(style.font)
-	text:setRect(-320,-240,320,240)
-	text:setAlignment(MOAITextBox.CENTER_JUSTIFY,MOAITextBox.CENTER_JUSTIFY)
-	b.layer:insertProp(text)
-	b.textbox = text]]
-	--[[local box = GlowBoxActor()
-	box.style = self:getStyle('GlowBoxActor')
-	box.x,box.y = self.x,self.y
-	box.w,box.h = self.w,self.h
-	box:load()]]
-
-	self:setBorderStyle'edge'
 	self.text:load()
 	self:addActor(self.text)
 end
