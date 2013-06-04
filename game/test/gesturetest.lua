81f764c05f3b59e2b4a8634e919b02197439508f
Leon Jiang <titangate1202@gmail.com>
2013-06-04 08:04:33 -0700

    Update tests

--- game/test/gesturetest.lua
+++ game/test/gesturetest.lua
@@ -1,10 +1,11 @@
 require 'ui.widget'
 return function()
+    --MOAIGfxDevice.setClearColor(1,0.41,0.70,1)
 	local base = Widget.init(0,0,screenWidth,screenHeight)
     local b = Widget()
     b.x,b.y = 0,0
     b.w,b.h = 960,338
-    b:load()
+    b:load(base)
     b:setBackgroundImage("banner.png")
 
     base:addWidget(b)
@@ -12,7 +13,7 @@ return function()
     local c = Widget()
     c.x,c.y = 300,0
     c.w,c.h = 128,128
-    c:load()
+    c:load(b)
     c:setBackgroundImage("Icon_128.png")
     --c:setPosition(300,200)
 
@@ -21,32 +22,25 @@ return function()
     assert(POE_CONTROLSCHEME == 'mobile',"not running on mobile device")
     local tg = TransformGestureRecognizer()
 
-    base:addGestureRecognizer(tg)
-    function tg.onGestureRecognizerFinished()
+    b:addGestureRecognizer(tg)
+    tg:registerEventHandler('GestureFinished',function()
         print 'gesture finished'
-    end
+    end)
 
-    function tg.onGestureRecognizerFailed()
+    tg:registerEventHandler('GestureFailed',function()
         print 'gesture failed'
-    end
+    end)
 
-    function tg.onGestureRecognizerChanged(recognizer)
+    tg:registerEventHandler('GestureChanged',function(recognizer)
         b:setPosition(recognizer:getPosition())
         b:setAngle(recognizer:getAngle())
         b:setScale(recognizer:getScale(),recognizer:getScale())
-        --b:updateBackgroundImage()
-    end
+    end)
 
     local tgtap = TapGestureRecognizer()
-    base:addGestureRecognizer(tgtap)
+    b:addGestureRecognizer(tgtap)
     tgtap:requireGesturesToFail(tg)
 
-    function tgtap:onGestureRecognizerFinished()
-        print 'tapped'
-        if b:inBound(self:getPosition()) then
-            b.backgroundImage:loadShader(Shader.vibrate(b.backgroundImage))
-        end
-    end
     function tgtap:onGestureRecognizerChangedState(state)
         print (state)
     end
@@ -55,6 +49,43 @@ return function()
         print 'gesture failed'
     end
 
+    local function renderTest(frameBuffer)
+
+    gfxQuad = MOAIGfxQuad2D.new ()
+    gfxQuad:setTexture ( 'banner.png' )
+    gfxQuad:setRect ( -128, -128, 128, 128 )
+    gfxQuad:setUVRect ( 0, 0, 1, 1 )
+
+    viewport = MOAIViewport.new ()
+    viewport:setSize ( 320, 480 )
+    viewport:setScale ( 320, -480 )
+
+    layer = MOAILayer2D.new ()
+    layer:setViewport ( viewport )
+    MOAISim.pushRenderPass ( layer )
+
+    prop = MOAIProp2D.new ()
+    prop:setDeck ( gfxQuad )
+    layer:insertProp ( prop )
+    prop:moveRot ( -180, 1.5 )
+
+        MOAIThread.blockOnAction ( prop:moveRot ( 360, 1.5 ))
+        MOAIThread.blockOnAction ( prop:moveRot ( -360, 1.5 ))
+        print 'hmm'
+    end
+
     MOAIRenderMgr.pushRenderPass(base.layer)
     base:enableDebugProp(true)
+        local d = Widget()
+        d.x,d.y = 100,100
+        d.w,d.h = 960,338
+        d:load(base)
+        base:addWidget(d)
+    t = 0
+    tgtap:registerEventHandler('GestureFinished',function(recognizer,event)
+        --renderTest()
+        b:vibrateIn()
+        --b:transitionIn()
+    end)
+    
 end
--- game/test/widgettest.lua
+++ game/test/widgettest.lua
@@ -1,29 +1,43 @@
-return function()
-	local base = Widget.init(0,0,screenWidth,screenHeight)
-    local b = Widget()
-    b.x,b.y = 0,0
-    b.w,b.h = 960,338
-    b:load(base)
-    b:setBackgroundImage("banner.png")
-
-    base:addWidget(b)
-
-    local c = Widget()
-    c.x,c.y = 300,0
-    c.w,c.h = 128,128
-    c:load(base)
-    c:setBackgroundImage("Icon_128.png")
-
-    b:addWidget(c)
-
+local function buttonTestPanel(base)
+    local panelbase = Widget()
+    panelbase.w,panelbase.h = 600,600
+    panelbase.x,panelbase.y = 100,0
+    panelbase:load(base)
+    panelbase:setBorderStyle'box'
+    base:addWidget(panelbase)
+    local panel = Table()
+    panel.w,panel.h = 600,600
+    panel.x,panel.y = 0,0
+    panel:load(panelbase)
+    panelbase:addWidget(panel)
+    panel:setTitle'BUTTON TEST'
     local button = Button()
     button.x,button.y = -0,-0
-    button.w,button.h = 400,50
-    button:load(base)
-    button:setTitle'<c:00ffaa>COME AT ME BRO<>'
+    button.w,button.h = 300,38
+    button:load(panel)
+    panel:addWidget(button)
+    button:setBorderStyle'edge'
+    button:setTitle'COME AT ME BRO'
+    button:registerEventHandler('Click',function( )
+            print 'button pressed'
+        end)
 
-    base:addWidget(button)
+    local textbox = TextBox()
+    textbox.x,textbox.y = 0,100
+    textbox.w,textbox.h = 300,48
+    textbox:load(panel)
+    panel:addWidget(textbox)
+    panel:vibrateIn()
+    --panel:setPosition(0,0,2)
+    --panelbase:setContentOffset(-300,0,3)
+    --panelbase:transitionIn()
+    --panelbase:setPosition(-300,0,10)
+end
 
+return function()
+	local base = Widget.init(0,0,screenWidth,screenHeight)
+    buttonTestPanel(base)
+    base.layer:setClearColor(1,1,1,1)
     MOAIRenderMgr.pushRenderPass(base.layer)
-    base:enableDebugProp(true)
+    --base:enableDebugProp(true)
 end
