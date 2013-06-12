0c6f3fa797290133e6ce5f63f23b6f12e8e3a9e3
Leon Jiang <titangate1202@gmail.com>
2013-06-11 23:24:29 -0700

    New widget test cases and adds widget update loop

--- game/test/widgettest.lua
+++ game/test/widgettest.lua
@@ -1,16 +1,16 @@
 local function buttonTestPanel(base)
-    local panelbase = Widget()
+    local panelbase = Table()
     panelbase.w,panelbase.h = 600,600
-    panelbase.x,panelbase.y = 100,0
+    panelbase.x,panelbase.y = 300,0
     panelbase:load(base)
     panelbase:setBorderStyle'box'
+    panelbase:setTitle'BUTTON TEST'
     base:addWidget(panelbase)
-    local panel = Table()
+    local panel = TableContent()
     panel.w,panel.h = 600,600
     panel.x,panel.y = 0,0
     panel:load(panelbase)
     panelbase:addWidget(panel)
-    panel:setTitle'BUTTON TEST'
     local button = Button()
     button.x,button.y = -0,-0
     button.w,button.h = 300,38
@@ -26,12 +26,14 @@ local function buttonTestPanel(base)
     textbox.x,textbox.y = 0,100
     textbox.w,textbox.h = 300,48
     textbox:load(panel)
+    textbox:setBorderStyle'edge'
     panel:addWidget(textbox)
-    panel:vibrateIn()
+    --panel:vibrateIn()
     --panel:setPosition(0,0,2)
-    --panelbase:setContentOffset(-300,0,3)
+    panel:setContentOffset(-300,0,3)
     --panelbase:transitionIn()
-    --panelbase:setPosition(-300,0,10)
+    --panelbase:setPosition(-300,0,5)
+    button:setSize(400,38,1)
 end
 
 return function()
@@ -39,5 +41,18 @@ return function()
     buttonTestPanel(base)
     base.layer:setClearColor(1,1,1,1)
     MOAIRenderMgr.pushRenderPass(base.layer)
-    --base:enableDebugProp(true)
+        
+    local prevElapsedTime = MOAISim.getDeviceTime()
+    local elapsedTime = 0
+    local thread = MOAICoroutine.new()
+    thread:run(
+        function()
+            while (true) do
+                local currElapsedTime = MOAISim.getDeviceTime()
+                elapsedTime = currElapsedTime - prevElapsedTime
+                prevElapsedTime = currElapsedTime
+                Widget.base:update(elapsedTime)
+                coroutine.yield()
+            end
+        end)
 end
